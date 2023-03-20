f59433352293aa1add2cd129c616353b0d3d69f4
javycito <brrayanperez10@gmail.com>
2023-03-19 22:04:55 -0400

    Added Wellcome message

--- src/js/controller.js
+++ src/js/controller.js
@@ -145,5 +145,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlUploadRecipe);
+  console.log('Wellcome to my site <3');
 };
 init();
